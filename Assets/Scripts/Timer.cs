cf55c079f3b08bcf1ad351cfa7d90f419b667cd7
Kaitlyn Deppmeier <kdeppmeier@yahoo.com>
2017-04-22 12:29:12 -0700

    Started timer script

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    //The timer text
+    public GameObject timer;
+    //The player
+    public GameObject player;
+
+    //The number of seconds the player has to complete the level
+    public float secondsToComplete;
+
+	// Use this for initialization
+	void Start ()
+    {
+		if ( player.GetComponent<PlayerController>() == null)
+        {
+            Debug.Log("Timer script lacks a player with PlayerController");
+        }
+
+        if ( timer.GetComponent<Text>() == null)
+        {
+            Debug.Log("Timer script lacks a timer with Text (UI)");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (secondsToComplete <= 0)
+        {
+            player.GetComponent<PlayerController>().enabled = false;
+            Debug.Log("Level failed");
+        }
+        else
+        {
+            secondsToComplete -= Time.deltaTime;
+            timer.GetComponent<Text>().text = secondsToComplete.ToString();
+        }
+	}
+}
