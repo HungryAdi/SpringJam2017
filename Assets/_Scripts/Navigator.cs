0e5ef0fba5407ebc3f355a83e883b731d987bbf8
Billy Ismanto <bismanto@uci.edu>
2017-04-22 11:44:13 -0700

    SceneManagement
    Adding NewGame button from "Billy" scene that connects to
    "LevelSelection" scene

---
+++ Assets/_Scripts/Navigator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Navigator : MonoBehaviour {
+    //singleton
+    public static Navigator Instance;
+
+    // Use this for initialization
+    void Start () {
+        if (!Instance)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void LoadScene(string action)
+    {
+        // Loads the scene based on the given string, or exits the game if the string is Quit
+        if (action == "Quit")
+        {
+            Application.Quit();
+        }
+        else
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(action);
+        }
+    }
+}
