681c1816739543b71ea69933232843bfdbf37f29
Kaitlyn Deppmeier <kdeppmeier@yahoo.com>
2017-04-22 11:25:37 -0700

    Camera script - basic follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    //The object the camera will follow
+    //Might be an empty child object of the player
+    public GameObject followPoint;
+
+    void Start()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //The position of the point the camera follows
+        Vector3 followPointPos = followPoint.transform.position;
+        //Camera keeps its z position - follows x and y of the follow point
+        transform.position = new Vector3( followPointPos.x, followPointPos.y, transform.position.z) ;
+	}
+}
