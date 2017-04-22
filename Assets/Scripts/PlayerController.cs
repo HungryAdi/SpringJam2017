5882d17a24b7015b16fffb941c85bb9db867b3a0
Kaitlyn Deppmeier <kdeppmeier@yahoo.com>
2017-04-22 12:15:06 -0700

    Converted player to rigidbody

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,11 +18,14 @@ public class PlayerController : MonoBehaviour
 
     //Use CharacterController?  Or Rigidbody for physics?
     private CharacterController charControl;
+    //Player's rigidbody
+    private Rigidbody rBody;
 
 	// Use this for initialization
 	void Start ()
     {
         charControl = gameObject.GetComponent<CharacterController>();
+        rBody = gameObject.GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
@@ -34,40 +37,28 @@ public class PlayerController : MonoBehaviour
     void MovePlayer ()
     {
         float vertical = Input.GetAxis( "Vertical" );
-        if ( !isJumping )
+        //If player pressed jump key (up/w) and the player is grounded (Raycast checks if they are close enough to the ground)
+        if (vertical > 0.1 && Physics.Raycast(transform.position, Vector3.down, 0.75F))
         {
-            //If player pressed jump key (up/w) and the player is grounded (Raycast checks if they are close enough to the ground)
-            if (vertical > 0.1 && Physics.Raycast(transform.position, Vector3.down, 0.75F))
-            {
-                //Debug.Log( "jump" );
-                isJumping = true;
-                jumpStart = transform.position;
-                Jump( jumpStart );
-            }
-            //Player is not jumping
-            else
-            {
-                //"Gravity" will take over, bringing the character back down.
-                charControl.Move( (Vector3.down * vertSpeed + Vector3.right * horizSpeed) * Time.deltaTime);
-            }
+            //Debug.Log( "jump" );
+            //isJumping = true;
+            jumpStart = transform.position;
+            Jump( jumpStart );
         }
+        //Player is not jumping
         else
         {
-            Jump(jumpStart);
+            //"Gravity" will take over, bringing the character back down.
+
+            rBody.MovePosition( transform.position + (Vector3.right * horizSpeed) * Time.deltaTime );
         }
     }
 
     //Rudimentary jump mechanics - may change
     void Jump ( Vector3 start )
     {
-        //Once the player reaches the jump height, the player will come back down
-        if (transform.position.y - start.y < jumpHeight)
-        {
-            charControl.Move( (Vector3.up * vertSpeed + Vector3.right * horizSpeed) * Time.deltaTime);
-        }
-        else
-        {
-            isJumping = false;
-        }
+        //Debug.Log("JUMPING");
+        rBody.MovePosition( transform.position + (Vector3.right * horizSpeed) * Time.deltaTime );
+        rBody.velocity = rBody.velocity + Vector3.up * vertSpeed;
     }
 }
