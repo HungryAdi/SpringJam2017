0dfe7647e303a465c59f25d063f701891e078924
Kaitlyn Deppmeier <kdeppmeier@yahoo.com>
2017-04-22 23:19:16 -0700

    Edited spike prefab

--- Assets/Scripts/SpikeAttack.cs
+++ Assets/Scripts/SpikeAttack.cs
@@ -4,17 +4,31 @@ using UnityEngine;
 
 public class SpikeAttack : MonoBehaviour
 {
+    //Will the flame start out as big?
+    public bool startMax;
+
     //The part of the spikes that scales
     GameObject spikes;
     //Whether the "spikes" are growing or retracting
     int spikeGrowth;
 
+    Vector3 origSize;
 
 	// Use this for initialization
 	void Start ()
     {
         spikes = gameObject.transform.FindChild("Spikes").gameObject;
-        spikeGrowth = 1;
+        origSize = Vector3.zero + spikes.transform.localScale;
+
+        if (!startMax)
+        {
+            spikeGrowth = 1;
+        }
+        else
+        {
+            spikeGrowth = -1;
+            spikes.transform.localScale = new Vector3 (origSize.x, 2 * origSize.y, origSize.z);
+        }
 
         IEnumerator coroutine = SpikeMovement();
         StartCoroutine(coroutine);
@@ -31,9 +45,26 @@ public class SpikeAttack : MonoBehaviour
         while(true)
         {
             yield return new WaitForSeconds(1F);
-            spikeGrowth = -1;
+            spikeGrowth = -spikeGrowth;
+            if (spikeGrowth == 1)
+            {
+                spikes.transform.localScale = origSize;
+            }
+            else
+            {
+                spikes.transform.localScale = new Vector3(origSize.x, 2 * origSize.y, origSize.z);
+            }
+            //spikes.transform.localScale += new Vector3(0, 1, 0) * spikeGrowth;
             yield return new WaitForSeconds(1F);
-            spikeGrowth = 1;
+            spikeGrowth = -spikeGrowth;
+            if (spikeGrowth == 1)
+            {
+                spikes.transform.localScale = origSize;
+            }
+            else
+            {
+                spikes.transform.localScale = new Vector3(origSize.x, 2 * origSize.y, origSize.z);
+            }
         }
     }
 }
